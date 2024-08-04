9fc39c3a953e7016204c6d5ed2e3a2ce415b340c
reyhoondavaee@gmail.com <reyhoondavaee@gmail.com>
2024-08-04 12:30:55 +0330

    crud added

---
+++ crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+from typing import List
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 10, sort_by: str = 'username'):
+    return db.query(models.User).order_by(getattr(models.User, sort_by)).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(username=user.username, email=user.email, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, user_id: int, user: schemas.UserUpdate):
+    db_user = get_user(db, user_id)
+    if db_user:
+        for key, value in user.dict().items():
+            setattr(db_user, key, value)
+        db.commit()
+        db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = get_user(db, user_id)
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
